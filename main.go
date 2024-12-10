1df1792aba6ac8965037eb87b91c4d5e7cbec375
DongSeonYoo <inko51366@naver.com>
2024-12-10 20:24:39 +0000

    open server & rendered gohtml files

--- main.go
+++ main.go
@@ -2,19 +2,57 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 
 	"github.com/DongSeonYoo/go-coin/blockchain"
 )
 
-func main() {
-	chain := blockchain.GetBlockChain()
-	chain.AddBlock("secondBlock")
-	chain.AddBlock("thirdBlock")
-	chain.AddBlock("fourBlock")
+const (
+	templateDir string = "templates/"
+	port        string = ":4000"
+)
 
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+var templates *template.Template
+
+// render our block
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	data := homeData{PageTitle: "Home", Blocks: blockchain.GetBlockChain().AllBlocks()}
+
+	if err := templates.ExecuteTemplate(w, "home", data); err != nil {
+		panic(err)
 	}
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		if err := templates.ExecuteTemplate(rw, "add", nil); err != nil {
+			panic(err)
+		}
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			panic(err)
+		}
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func main() {
+	templates = template.Must(templates.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s", port)
+
+	log.Fatal(http.ListenAndServe(port, nil))
 }
